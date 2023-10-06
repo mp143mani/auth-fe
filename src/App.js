751c7949cf71e2da49234ebb4929bb25cdcf86e8
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-10-06 20:48:24 +0530

    sss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Dashboard from './components/Dashboard'
+import SignUp from './components/SignUp'
+import Signin from './components/Signin'
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 
 function App() {
+  // console.log(process.env.REACT_APP_API_URL)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+            <Route path='/sign-in' element={<Signin/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+            <Route path='/*' element={<Navigate to="/sign-in"/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
